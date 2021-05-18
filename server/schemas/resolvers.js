c5c50d355849b74b4a90c0a00de37f039d582f28
Matt Gross <mbgross111@gmail.com>
2021-05-18 14:48:53 -0700

    add  server, auth, basic User model, query: user, mutations: createUser and login

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,42 @@
+const { AuthenticationError } = require('apollo-server-express');
+const { User } = require('../models');
+const { signToken } = require('../utils/auth');
+// const stripe = require('stripe')('sk_test_4eC39HqLyjWDarjtT1zdp7dc');
+
+const resolvers = {
+  Query: {
+    user: async (parent, args, context) => {
+      if (context.user) {
+        const user = await User.findById(context.user._id);
+        return user
+      }
+      throw new AuthenticationError('Not logged in');
+    },
+
+    // addMoreQueries {},
+  },
+  Mutation: {
+    createUser: async (parent, args) => {
+      const user = await User.create(args);
+      const token = signToken(user);
+      return { token, user };
+    },
+
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+      if (!user) {
+        throw new AuthenticationError('Incorrect login credentials');
+      }
+      const validPassword = await user.validatePassword(password);
+      if (!validPassword) {
+        throw new AuthenticationError('Incorrect login credentials');
+      }
+      const token = signToken(user);
+      return { token, user };
+    }
+  },
+  
+  // addMoreMutations {},
+}
+
+module.exports = resolvers;
---
+++ server/utils/auth.js
@@ -0,0 +1,32 @@
+const jwt = require('jsonwebtoken');
+
+const secret = process.env.JWT_SECRET || 'bigSecret';
+const expiration = '4h'
+
+module.exports = {
+  authMiddleware: ({ req }) => {
+    let token = req.headers.authorization || req.body.token || req.query.token;
+
+    if (req.headers.authorization) {
+      token = token.split(' ').pop().trim();
+    }
+
+    if (!token) {
+      return req;
+    }
+
+    try {
+      const { data } = jwt.verify(token, secret, { maxAge: expiration });
+      req.user = data;
+    } catch {
+      console.log('Invalid Token');
+    }
+
+    return req;
+  },
+  signToken: ({ email, _id }) => {
+    const payload = { email, _id };
+
+    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+  }
+}
