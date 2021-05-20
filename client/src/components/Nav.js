4a1eadd86b18aacf5a940c0e47702d9a8dc80620
Matt Gross <mbgross111@gmail.com>
2021-05-19 18:57:22 -0700

    add file uploader and display for vendor logos

---
+++ client/src/components/Nav.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Auth from "../utils/auth";
+import { Link } from "react-router-dom";
+
+function Nav() {
+  return (
+    <header>
+      <h1 style={{textAlign: "center"}}>🌐 HELLO FROM BUY LOCAL 🌐</h1>
+    </header>
+  );
+}
+
+export default Nav;
