3513979de9d7005b36edb3edbc51e61f1fd70b32
Alexander Giannini <alexgiannini@alexanders-mbp.home>
2021-05-21 14:34:32 -0700

    added additional queries/mutations to FE

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -28,7 +28,7 @@ export const GET_USER = gql`
 
 export const GET_SHOPS = gql`
     {
-        getShops {
+        shops {
             _id
             name
             description
@@ -90,6 +90,102 @@ export const GET_SHOPS = gql`
             reviewCount
             ratingCount
             ratingAvg
+        }
+    }
+`;
+
+export const GET_SHOP_BY_ID = gql`
+    query getShopById($_id: ID!) {
+        shop(_id: $_id) {
+            _id
+            name
+            description
+            instagram
+            logo
+            phone
+            addressNum
+            street
+            city
+            state
+            zip
+            stripeKey
+            stripeKeyVerified
+            open
+            pickup
+            delivery
+            shipping
+            owner {
+                _id
+                firstName
+                lastName
+                email
+            }
+            categories {
+                _id
+                name
+            }
+            products {
+                _id
+                name
+                description
+                price
+                image
+                stock
+                category {
+                    _id
+                    name
+                }
+            }
+            sales {
+                purchases {
+                    purchaseQuantity
+                    product {
+                        _id
+                        name
+                    }
+                }
+            }
+            ratings {
+                createdAt
+                stars
+                user {
+                    firstName
+                }
+            }
+            reviews {
+                createdAt
+                reviewText
+                user {
+                    _id
+                }
+            }
+            categoryCount
+            reviewCount
+            ratingCount
+            ratingAvg
+
+        }
+    }
+`;
+
+export const GET_CATEGORIES = gql`
+    {
+        categories {
+            name
+        }
+    }
+`;
+
+
+export const GET_PRODUCTS = gql`
+    {
+        products {
+            _id
+            name
+            description
+            price
+            image
+            stock
         }
     }
 `;
