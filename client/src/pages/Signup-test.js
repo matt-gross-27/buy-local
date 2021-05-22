fa8c68987a60743aa9ede96ad7ee16660c6a0d46
Davit Shirkhanyan <davit.shirkhanyan@gmail.com>
2021-05-21 21:19:15 -0700

    created variables for signup page

--- client/src/pages/Signup-test.js
+++ client/src/pages/Signup-test.js
@@ -3,3 +3,13 @@ import { Link } from "react-router-dom";
 import { useMutation } from '@apollo/react-hooks';
 import Auth from "../utils/auth";
 import { CREATE_USER } from "../utils/mutations";
+
+function Signup(props) {
+    const [formState, setFormState] = useState({
+        email: '',
+        password: ''
+    });
+    const [createUser] = useMutation(CREATE_USER);
+};
+
+export default Signup;
