de4491034d1f5465f4356d1cde785e7242376baa
Davit Shirkhanyan <davit.shirkhanyan@gmail.com>
2021-05-23 02:46:08 -0700

    added github logo and links

--- client/src/components/footers/SimpleFiveColumn.js
+++ client/src/components/footers/SimpleFiveColumn.js
@@ -5,7 +5,7 @@ import { css } from "styled-components/macro"; //eslint-disable-line
 
 import LogoImage from "../../images/logo.svg";
 import { ReactComponent as FacebookIcon } from "../../images/facebook-icon.svg";
-import { ReactComponent as TwitterIcon } from "../../images/twitter-icon.svg";
+import { ReactComponent as GithubIcon } from "../../images/github.svg";
 import { ReactComponent as YoutubeIcon } from "../../images/youtube-icon.svg";
 
 const Container = tw.div`relative bg-gray-200 -mx-8 -mb-8 px-8`;
@@ -40,20 +40,20 @@ export default () => {
       <FiveColumns>
         <WideColumn>
           <LogoContainer>
-            <LogoImg src={LogoImage} />
-            <LogoText>Treact Inc.</LogoText>
+            {/* <LogoImg src={LogoImage} /> */}
+            {/* <LogoText>Treact Inc.</LogoText> */}
           </LogoContainer>
           <CompanyDescription>
-            Treact is an Internet Technology company providing design resources such as website templates and themes.
+            {/* Treact is an Internet Technology company providing design resources such as website templates and themes. */}
           </CompanyDescription>
           <SocialLinksContainer>
-            <SocialLink href="https://facebook.com">
+            <SocialLink href="https://facebook.com" target="_blank">
               <FacebookIcon />
             </SocialLink>
-            <SocialLink href="https://twitter.com">
-              <TwitterIcon />
+            <SocialLink href="https://github.com/matt-gross-27/buy-local" target="_blank">
+              <GithubIcon />
             </SocialLink>
-            <SocialLink href="https://youtube.com">
+            <SocialLink href="https://www.youtube.com/watch?v=TPszlDVZ9GA" target="_blank">
               <YoutubeIcon />
             </SocialLink>
           </SocialLinksContainer>
